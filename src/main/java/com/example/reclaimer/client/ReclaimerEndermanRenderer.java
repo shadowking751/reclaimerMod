3e70e8fbe4d228470b0c5e38978d16f8dc7e0243
shadowking751 <pancakes711@icloud.com>
2026-02-24 20:12:49 -0600

    Create ReclaimerEndermanRenderer.java

---
+++ src/main/java/com/example/reclaimer/client/ReclaimerEndermanRenderer.java
@@ -0,0 +1,23 @@
+package com.example.reclaimer.client;
+
+import com.example.reclaimer.ModEntities;
+import com.example.reclaimer.ReclaimerMod;
+import com.example.reclaimer.entity.ReclaimerEndermanEntity;
+import net.minecraft.client.render.entity.EndermanEntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.util.Identifier;
+
+public class ReclaimerEndermanRenderer extends EndermanEntityRenderer {
+
+    private static final Identifier TEXTURE =
+            new Identifier(ReclaimerMod.MOD_ID, "textures/entity/reclaimer_enderman.png");
+
+    public ReclaimerEndermanRenderer(EntityRendererFactory.Context ctx) {
+        super(ctx);
+    }
+
+    @Override
+    public Identifier getTexture(ReclaimerEndermanEntity entity) {
+        return TEXTURE;
+    }
+}
