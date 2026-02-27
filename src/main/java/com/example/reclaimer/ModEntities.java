db3c28517d8646b7e01dee5f236ae7332caa1700
shadowking751 <pancakes711@icloud.com>
2026-02-27 13:26:02 -0600

    Update ModEntities.java

--- src/main/java/com/example/reclaimer/ModEntities.java
+++ src/main/java/com/example/reclaimer/ModEntities.java
@@ -2,8 +2,9 @@ package com.example.reclaimer;
 
 import com.example.reclaimer.entity.ReclaimerEndermanEntity;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.*;
-import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
