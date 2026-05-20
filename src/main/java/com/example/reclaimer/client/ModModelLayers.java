6b79c5c66bf1aaaca317a61536432999858d56b7
shadowking751 <pancakes711@icloud.com>
2026-05-20 13:49:59 -0500

    Update ModModelLayers.java

--- src/main/java/com/example/reclaimer/client/ModModelLayers.java
+++ src/main/java/com/example/reclaimer/client/ModModelLayers.java
@@ -1,13 +1,14 @@
 package com.example.reclaimer.client;
 
+import net.minecraft.client.render.entity.model.EntityModelLayer;
+import net.minecraft.util.Identifier;
+
 import com.example.reclaimer.ReclaimerMod;
-import net.minecraft.client.model.geom.ModelLayerLocation;
-import net.minecraft.resources.ResourceLocation;
 
 public class ModModelLayers {
-    public static final ModelLayerLocation RECLAIMER_ENDERMAN =
-            new ModelLayerLocation(
-                    new ResourceLocation(ReclaimerMod.MOD_ID, "reclaimer_enderman"),
+    public static final EntityModelLayer RECLAIMER_ENDERMAN_LAYER =
+            new EntityModelLayer(
+                    new Identifier(ReclaimerMod.MOD_ID, "reclaimer_enderman"),
                     "main"
             );
 }
