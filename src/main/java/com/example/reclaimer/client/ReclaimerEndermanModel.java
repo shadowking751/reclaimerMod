e4ade1da76ac102e7f1c98972b2186f747bb5728
shadowking751 <pancakes711@icloud.com>
2026-05-20 13:50:19 -0500

    Update ReclaimerEndermanModel.java

--- src/main/java/com/example/reclaimer/client/ReclaimerEndermanModel.java
+++ src/main/java/com/example/reclaimer/client/ReclaimerEndermanModel.java
@@ -3,14 +3,26 @@ package com.example.reclaimer.client;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.model.EndermanEntityModel;
 import net.minecraft.client.render.entity.model.TexturedModelData;
+import net.minecraft.client.render.entity.model.EntityModelPartNames;
+import net.minecraft.client.model.Dilation;
+import net.minecraft.client.model.ModelData;
+import net.minecraft.client.model.ModelPartBuilder;
+import net.minecraft.client.model.ModelTransform;
+import net.minecraft.client.model.ModelPartData;
+
+import com.example.reclaimer.entity.ReclaimerEndermanEntity;
 
 public class ReclaimerEndermanModel extends EndermanEntityModel<ReclaimerEndermanEntity> {
-
     public ReclaimerEndermanModel(ModelPart root) {
         super(root);
     }
 
     public static TexturedModelData getTexturedModelData() {
-        return EndermanEntityModel.getTexturedModelData();
+        ModelData modelData = EndermanEntityModel.getModelData(new Dilation(0.0F));
+        ModelPartData root = modelData.getRoot();
+
+        // You can customize parts here if you want
+
+        return TexturedModelData.of(modelData, 64, 32);
     }
 }
